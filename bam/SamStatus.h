dfb1e762c59dfb98d0ce112016af72dead8792aa
mktrost <mktrost@umich.edu>
2011-09-06 17:51:13 -0400

    Update doxygen comments for SamStatus and SamValidation

--- bam/SamStatus.h
+++ bam/SamStatus.h
@@ -21,74 +21,77 @@
 #include <iostream>
 #include "ErrorHandler.h"
 
+/// This class is used to track the status results of some methods in the BAM
+/// classes. It contains a status enum that describing the status.
 class SamStatus
 {
 public:
 
-    // Return value enum for the SamFile class methods.
-    //    SUCCESS      : method completed successfully.
-    //    UNKNOWN      : unknown result (default value should never be used)
-    //    NO_MORE_RECS : failed to read a record since there are no more to read
-    //                   either in the file or section if section based reading.
-    //    FAIL_IO      : method failed due to an I/O issue.
-    //    FAIL_ORDER   : method failed because it was called out of order,
-    //                   like trying to read a file without opening it for
-    //                   read or trying to read a record before the header.
-    //    FAIL_PARSE   : failed to parse a record/header - invalid format.
-    //    INVALID_SORT : record is invalid due to it not being sorted.
-    //    INVALID      : record is invalid other than for sorting.
-    //    FAIL_MEM     : fail a memory allocation.
-    enum Status {SUCCESS = 0, UNKNOWN, NO_MORE_RECS, FAIL_IO, FAIL_ORDER,
-                 FAIL_PARSE, INVALID_SORT, INVALID, FAIL_MEM};
+    /// Return value enum for the SamFile class methods.
+    enum Status 
+        { SUCCESS = 0, ///< method completed successfully.
+          UNKNOWN, ///< unknown result (default value should never be used)
+          /// NO_MORE_RECS: failed to read a record since there are no more to
+          /// read either in the file or section if section based reading.
+          NO_MORE_RECS,
+          FAIL_IO, ///< method failed due to an I/O issue.
+          /// FAIL_ORDER: method failed because it was called out of order,
+          /// like trying to read a file without opening it for read or trying
+          /// to read a record before the header.
+          FAIL_ORDER,
+          FAIL_PARSE, ///< failed to parse a record/header - invalid format.
+          INVALID_SORT, ///< record is invalid due to it not being sorted.
+          INVALID, ///< record is invalid other than for sorting.
+          FAIL_MEM ///< fail a memory allocation.
+        };
 
+    /// Return a string representation of the passed in status enum.
     static const char* getStatusString(SamStatus::Status statusEnum);
 
-    // Returns whether or not it is "safe" to keep processing the file
-    // after the specified status return.
+    /// Returns whether or not it is "safe" to keep processing the file
+    /// after the specified status return.
     static bool isContinuableStatus(SamStatus::Status status);
 
-    // Constructor
+    /// Constructor that takes in the handling type, defaulting it to exception.
     SamStatus(ErrorHandler::HandlingType handleType = ErrorHandler::EXCEPTION);
    
-    // Destructor
+    /// Destructor
     ~SamStatus();
 
-    // Resets this status.
+    /// Reset this status to a default state.
     void reset();
 
+    /// Set how to handle the errors when they are set.
     void setHandlingType(ErrorHandler::HandlingType handleType);
 
-    // Set the status with the specified values.
+    /// Set the status with the specified status enum and message.
     void setStatus(Status newStatus, const char* newMessage);
 
-    // Adds the specified error message to the status message.
-    // Sets the status to newStatus if the current status is SUCCESS.
+    /// Add the specified error message to the status message, setting
+    /// the status to newStatus if the current status is SUCCESS.
     void addError(Status newStatus, const char* newMessage);
 
 
-    // Adds the specified status to the status message.
-    // Sets the status to newStatus if the current status is SUCCESS.
+    /// Add the specified status to the status message, setting
+    /// the status to newStatus if the current status is SUCCESS.
     void addError(SamStatus newStatus);
 
-    // Return the enum for this status.
+    /// Return the enum for this status object.
     Status getStatus() const;
 
-    // Return the status message.
+    /// Return the status message for this object.
     const char* getStatusMessage() const;
 
-    // Overload operator = to set the sam status type to the
-    // passed in status and to clear the message string.
+    /// Overload operator = to set the sam status type to the
+    /// passed in status and to clear the message string.
     SamStatus & operator = (Status newStatus);
-   
-    //    // Overload operator = to set the sam status.
-    //    SamStatus & operator = (SamStatus newStatus);
-   
-    // Overload operator != to determine if the passed in type is not equal
-    // to this status's type.
+
+    /// Overload operator != to determine if the passed in type is not equal
+    /// to this status's type.
     bool operator != (const SamStatus::Status& compStatus) const;
 
-    // Overload operator == to determine if the passed in type is equal
-    // to this status's type.
+    /// Overload operator == to determine if the passed in type is equal
+    /// to this status's type.
     bool operator == (const SamStatus::Status& compStatus) const;
       
 private:
